228dea0bc6cd6268a09c5fcd83b8d3a0243ac5bf
Denis Platonov <deniplatonoff@yandex.ru>
2022-03-30 14:00:13 +0300

    feat: начать писать js

--- js/skript.js
+++ js/skript.js
@@ -1,1 +1,16 @@
-console.log('Hello, word!');
+const aboutProjektLink = document.querySelector('.profile__button-popup');
+const formWindow = document.querySelector('.popup');
+const formClose = formWindow.querySelector('.popup__close');
+
+function toggleFormWindow() {
+    formWindow.classList.toggle('popup_is-opened');
+}
+aboutProjektLink.addEventListener('click', toggleFormWindow);
+formClose.addEventListener('click', toggleFormWindow);
+
+function onOverLayClick(event) {
+    if (event.target === event.currentTarget) {
+        toggleFormWindow();
+    }
+}
+formWindow.addEventListener('click', onOverLayClick);
