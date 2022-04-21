cd7c5d41268f8c9dcf569a63b29d4a282d164c62
Bolgrad1990 <98160516+Bolgrad1990@users.noreply.github.com>
2022-04-21 19:17:25 +0300

    fix js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-const initialCards = [
-    {
+"use strict";
+const initialCards = [{
         name: 'Архыз',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
     },
@@ -24,12 +24,14 @@ const initialCards = [
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
+
+
 const aboutProjektLink = document.querySelector('.profile__button-popup');
 const cardsAddBtn = document.querySelector('.profile__button-add');
 const formElement = document.querySelector('[name="editForm"]');
 const formCardsAdd = document.querySelector('[name="cardsForm"]')
-const nameInputForm = formElement.querySelector('.form__input_type_name');
-const aboutMeInput = formElement.querySelector('.form__input_type_job');
+const nameInputForm = document.querySelector('.form__input_type_name');
+const aboutMeInput = document.querySelector('.form__input_type_job');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 const modalWindowEdit = document.querySelector('.popup_type_edit');
@@ -40,8 +42,9 @@ const cardCloseBtn = modalWindowCards.querySelector('.form__close_type_cards');
 const imageCloseBtn = modalWindowImage.querySelector('.form__close_type_image');
 const listContainer = document.querySelector('.list');
 const template = document.querySelector('.template');
-const cardImage = modalWindowImage.querySelector('.popup__image');
-const cardCaption = modalWindowImage.querySelector('.popup__caption');
+const cardImage = document.querySelector('.popup__image');
+const cardCaption = document.querySelector('.popup__caption');
+
 
 function openPopup(modalWindowEdit) {
     modalWindowEdit.classList.add('popup_is-opened');
@@ -51,7 +54,9 @@ function closePopup(modalWindowEdit) {
     modalWindowEdit.classList.remove('popup_is-opened');
 };
 
+
 aboutProjektLink.addEventListener('click', function() {
+    /*formWindow.classList.add('popup_is-opened');*/
     nameInputForm.value = profileTitle.textContent;
     aboutMeInput.value = profileSubtitle.textContent;
     openPopup(modalWindowEdit);
@@ -64,14 +69,13 @@ cardsAddBtn.addEventListener('click', () => {
 editCloseBtn.addEventListener('click', () => {
     closePopup(modalWindowEdit);
 });
-
 cardCloseBtn.addEventListener('click', () => {
     closePopup(modalWindowCards);
 });
-
 imageCloseBtn.addEventListener('click', () => {
     closePopup(modalWindowImage);
 });
+
 
 function onSubmit(evt) {
     evt.preventDefault();
@@ -79,6 +83,7 @@ function onSubmit(evt) {
     profileSubtitle.textContent = aboutMeInput.value;
     closePopup(modalWindowEdit);
 };
+
 
 function render() {
     const html = initialCards.map(getElement);
@@ -92,6 +97,7 @@ function getElement(item) {
     const imageLikeBtn = getElementTemplate.querySelector('.list__symbol');
     const imageDelBtn = getElementTemplate.querySelector('.list__delete');
 
+
     name.textContent = item.name;
     link.src = item.link;
     link.alt = item.name;
@@ -99,10 +105,8 @@ function getElement(item) {
     function removeElement(evt) {
         const element = evt.target.closest('.list__element');
         element.remove();
-    };
-
+    }
     imageDelBtn.addEventListener('click', removeElement);
-
     imageLikeBtn.addEventListener('click', function(evt) {
         evt.target.classList.toggle('element__symbol_active')
     });
@@ -125,10 +129,25 @@ function imageFormAddSubmitHandler(evt) {
     listContainer.prepend(element);
     closePopup(modalWindowCards);
     formCardsAdd.reset();
+
 };
 
 formElement.addEventListener('submit', onSubmit);
 formCardsAdd.addEventListener('submit', imageFormAddSubmitHandler);
 
+
 render();
 
+
+/*myForm.addEventListener('submit', onSubmit);
+formClose.addEventListener('click', closeModalWindow);*/
+
+/*function closeModalWindow() {
+    formWindow.classList.remove('popup_is-opened');
+};*/
+
+
+// Находим шаблон карточки Template и получаем его содержимое
+const cardTemplate = document.querySelector('#card-template').content;
+// Находим блок в котором будет использован Template
+const cardGrid = document.querySelector('.card-grid');
